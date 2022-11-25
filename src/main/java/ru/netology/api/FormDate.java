be235107cbe74a16241a1252f0703dce0ab2ed30
nasiabuslikprog <nasiabuslikprog@yandex.ru>
2022-11-25 03:12:48 +0300

    object creation

---
+++ src/main/java/ru/netology/api/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology.api;
+
+public class FormDate {
+
+    public int day;
+    public int month;
+    public int year;
+}
+
---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,18 @@
+package ru.netology.api;
+
+public class Post {
+    public  String name;
+    public  String password;
+    public String patronymic;
+    public  String phone;
+    public  String surname;
+    public Boolean subscription;
+
+    public FormDate birthday;
+
+
+
+
+
+}
+
